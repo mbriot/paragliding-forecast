63d0871207f3f37940dbde5ddb453de9cb7b6f52
arnaud <arnaud@MacBook-Pro-de-arnaud.local>
2022-10-14 23:55:45 +0200

    automate.sh publish website

--- automate.sh
+++ automate.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 function log() { echo -e "[shell-script][$(date '+%F %T')] $1";};
-[[ $(/usr/sbin/lsof -t $0| wc -l) -gt 1 ]] && log "At least one of $0 is running, exit now"
-[[ $(/usr/sbin/lsof -t $0| wc -l) -gt 1 ]] && exit 1
 log "Starting script"
 cd $1
 checkWeatherAnyway=$2
+
+export PATH=$PATH:/Users/arnaud/Desktop/paraglidingProd
 
 log "Cloning repository"
 git clone git@github.com:mbriot/paragliding-forecast.git
@@ -27,12 +27,18 @@ now=`date -jf "%d/%m/%YT%H:%M" $(date +%d/%m/%YT%H:%M) '+%s'`
 eightAmHourToday=`date -jf "%d/%m/%YT%H:%M" $(date +%d/%m/%YT08:00) '+%s'`
 sixPmHourToday=`date -jf "%d/%m/%YT%H:%M" $(date +%d/%m/%YT18:00) '+%s'`
 
-if [[ $now -gt $eightAmHourToday && $lastRunDay -lt $eightAmHourToday ]] || [[ $now -gt $sixPmHourToday && $lastRunDay -lt $sixPmHourToday ]]; then
+if [[ $now -gt $eightAmHourToday && $lastRunDay -lt $eightAmHourToday ]] || [[ $now -gt $sixPmHourToday && $lastRunDay -lt $sixPmHourToday ]] || [[ "x" != "x${checkWeatherAnyway}" ]]; then
   log "start checking weather"
   git co main
   source .venv/bin/activate
   pip3 install -r requirements.txt
-  python3 main.py --spot-file=../spots.json --config-file=../config.json --send-to-signal -v
+  python3 main.py --spot-file=spots.json --config-file=../config.json --send-to-website
+  ls -l ind*
+  cp index.markdown /Users/arnaud/Dev/workspace/parapente-dans-le-nord.github.io
+  cd /Users/arnaud/Dev/workspace/parapente-dans-le-nord.github.io
+  git commit -am "commit prediction"
+  GIT_SSH_COMMAND='ssh -i ~/.ssh/parapente-dans-le-nord' git push
+  cd ~/Desktop/paraglidingProd/paragliding-forecast
   git co automatization
   echo "OK,$(date +%d/%m/%YT%H:%M)" > status.txt
   git commit -am "update status"
