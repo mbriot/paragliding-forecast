5d49befa3f163ff2f4a02aa01855afaa0957f895
arnaud <arnaud@MacBook-Pro-de-arnaud.local>
2022-10-14 21:42:09 +0200

    first version jekyll

--- sender/websiteSender.py
+++ sender/websiteSender.py
@@ -1,6 +1,6 @@
-import click
-import json
 import logging
+from datetime import datetime
+from jinja2 import Environment, FileSystemLoader
 
 logger = logging.getLogger(__name__)
 
@@ -8,3 +8,13 @@ class WebsiteSender :
 
     def send(self, weekPrediction):
         logger.debug("je vais faire du html")
+        file_loader = FileSystemLoader('templates')
+        env = Environment(loader=file_loader)
+        template = env.get_template('index.j2')
+        template.globals['now'] = datetime.now().strftime('%A %d %B')
+        output = template.render(weekPrediction=weekPrediction)
+
+        f = open("index.markdown","w")
+        f.write(output)
+        f.close()
+        print(output)
